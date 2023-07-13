19c602be9bf28a459dffd12556682d243b7285fe
Camilo Mora, Ph.D. Professor University of Hawaii <moracamilo@hotmail.com>
2023-07-12 19:42:55 -0500

    pivot

--- MoneyManagementPlots.R
+++ MoneyManagementPlots.R
@@ -1,10 +1,20 @@
+library(plotly)
+library(rpivotTable)
+library( htmltools )
+Data=read.csv("D:/Scrips/Trading/OptiMoraR/MoneyManagementData.csv")
 
-Data=read.csv("D:/Scrips/Trading/Strategies/MoneyManagementData.csv")
+  
+  setwd("D:/Scrips/Trading/OptiMoraR")
+#As plot
+  #Data$Var <- do.call(paste, c(Data[c("ProfitTake", "ProfitTakeF", "FractionCapital", "ReEntry", "AdjustAfterNMartingale")], sep = " "))
+  #fig <- Data |plot_ly(x = ~PercentDecline, y = ~CummulativeLost , split = ~Var)
+  #fig <- fig %>% add_lines()
+  
+  
 
-Data$FractionCapital = sprintf("%e", Data$FractionCapital)
+Pivot=rpivotTable(Data, rows = c("AdjustAfterNMartingale" ,"ReEntry"), cols = c("PercentDecline"), aggregatorName = "Maximum", vals = "CummulativeLost",  inclusions = list(
+                     "ProfitTake" = list("0.15"),
+                     "ProfitTakeF" = list("0.1"),
+                     "FractionCapital" = list("5")))
 
-Data$Var <- do.call(paste, c(Data[c("ProfitTake", "ProfitTakeF", "FractionCapital", "ReEntry", "AdjustAfterNMartingale")], sep = " "))
-fig <- Data |> plot_ly(x = ~PercentDecline, y = ~CummulativeLost , split = ~Var)
-
-fig <- fig %>% add_lines()
-rpivotTable(Data, rows = c("PercentDecline"), cols = c("n_MG"), aggregatorName = "Max", vals = "CummulativeLost")
+save_html(Pivot, "MoneyManagementData.html", background = "white", libdir = "lib", lang = "en")
